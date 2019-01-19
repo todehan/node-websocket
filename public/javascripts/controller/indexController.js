b7f13613ad38a100a2004e94bc5deb5c9faf689d
todehan <todehan@gmail.com>
2019-01-19 13:18:59 +0300

    socket.io service

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,3 +1,11 @@
-app.controller("indexController", ["$scope", ($scope) => {
-    const socket = io.connect("http://localhost:3000");
+app.controller("indexController", ["$scope", "indexFactory", ($scope, indexFactory) => {
+    
+    indexFactory.connectSocket("http://localhost:3000", {
+        reconnectionAttempts: 3,
+        reconnectionDelay: 600        
+    }).then((socket) => {
+        console.log("Bağlantı Gerçekleşti", socket );
+    }).catch((err) => {
+        console.log("Hata Gerçekleşti", err)
+    });
 }]);
