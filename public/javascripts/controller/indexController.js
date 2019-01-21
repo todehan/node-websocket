0ccc95ddefbf98f3744b85e48417178b275a2832
todehan <todehan@gmail.com>
2019-01-22 02:25:53 +0300

    newUser event

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,11 +1,27 @@
 app.controller("indexController", ["$scope", "indexFactory", ($scope, indexFactory) => {
     
-    indexFactory.connectSocket("http://localhost:3000", {
+    $scope.init = () => {
+        const username = prompt("Please enter username");
+
+        if(username){
+            initSocket(username);
+        }
+        else{
+            return false;
+        }
+    };
+
+    function initSocket(username){
+
+        indexFactory.connectSocket("http://localhost:3000", {
         reconnectionAttempts: 3,
         reconnectionDelay: 600        
-    }).then((socket) => {
-        console.log("Bağlantı Gerçekleşti", socket );
-    }).catch((err) => {
+     }).then((socket) => {
+        socket.emit("newUser", {username});
+     }).catch((err) => {
         console.log("Hata Gerçekleşti", err)
-    });
+     });
+    }
+
+    
 }]);
