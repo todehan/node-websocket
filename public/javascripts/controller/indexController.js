9e7cc06ef7efecf973697349879bc57ee1a01627
todehan <todehan@gmail.com>
2019-01-23 01:44:37 +0300

    animate operations

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -52,6 +52,20 @@ app.controller("indexController", ["$scope", "indexFactory", ($scope, indexFacto
             $scope.messages.push(messagesData);
             $scope.$apply();
         });
+
+        let animate = false;
+        $scope.onClickPlayer = ($event) => {
+            
+            if (!animate){
+                
+                animate = true;
+                $("#"+ socket.id).animate({"left": $event.offsetX, "top": $event.offsetY}, () => {
+                    animate = false;
+                });
+            }
+
+        };
+
      }).catch((err) => {
         console.log("Hata Gerçekleşti", err)
      });
